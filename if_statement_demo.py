eea71d534f56126a584066ad01b6dfcf0520786b
Meinam01 <thoithoibameinam23@gmail.com>
2025-11-08 16:16:36 +0530

    Add files via upload

---
+++ if_statement_demo.py
@@ -0,0 +1,12 @@
+KD = 10
+
+if KD > 7:
+    print("pro player!")
+
+kill = 25
+if kill >= 20 :
+    print("you are pro player")
+
+name = "Meinam"
+if name == "Meinam":
+    print("welcome back, Meinam!")
---
+++ logical_operator.py
@@ -0,0 +1,15 @@
+a = 5
+b = 3
+
+# and operator
+print("a > 2 and b > 1:",a > 2 and b > 1) #true
+print("a > 2 and b > 5:",a > 2 and b > 5) #false
+
+# or operator
+print("a > 2 or b > 5:",a > 2 or b > 5) #true
+print("a < 1 or b < 0:",a < 1 or b < 0) #false
+
+# not operator
+print("not(a > 2):",not(a > 2)) #true
+print("not(a > 2):",not(a > 5)) #false
+
